90849e2097b20ba1b01087a51b3a6da4f89b1ce9
Kamel-Amen <kamelamen38@gamil.com>
2025-05-10 00:25:41 +0300

    fixing deploy process bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 // import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './Style.css';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
